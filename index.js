f7f57257fc1d2d4deb38ccd1ed2246bd12124e14
Sagar Kattel <71958289+sagarkattel@users.noreply.github.com>
2023-06-27 19:32:28 +0545

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://letschatus.netlify.app/",
+    origin: "https://letschatus.netlify.app",
     methods: ["GET", "POST"],
   },
 });
