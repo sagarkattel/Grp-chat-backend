7abbba262ed768b4345b083b48a57d0356406e87
sagarkattel <71958289+sagarkattel@users.noreply.github.com>
2023-06-27 20:37:10 +0545

    okkkkkk

--- index.js
+++ index.js
@@ -18,16 +18,10 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    const { username, room } = data;
+  socket.on("join_room", ({ room, username }) => {
     socket.join(room);
     console.log(`User with ID: ${socket.id} joined room: ${room}`);
-
-    // Emit the user_list event with the updated list of users in the room
-    io.to(room).emit("user_list", getUsersInRoom(room));
-
-    // You can also emit a welcome message or perform other actions as needed
-    io.to(room).emit("system_message", `Welcome, ${username}!`);
+    console.log(`Username: ${username}`);
   });
 
   socket.on("send_message", (data) => {
@@ -39,23 +33,10 @@ io.on("connection", (socket) => {
   });
 });
 
-// Helper function to get the list of users in a room
-function getUsersInRoom(room) {
-  const clients = io.sockets.adapter.rooms.get(room);
-  const users = [];
-  if (clients) {
-    for (const clientId of clients) {
-      const user = io.sockets.sockets.get(clientId);
-      users.push({ id: user.id, username: user.username });
-    }
-  }
-  return users;
-}
 
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
   res.send("Hello World");
-});
-
+})
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
 });
